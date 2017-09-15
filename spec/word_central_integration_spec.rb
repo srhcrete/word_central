65ab09be6f2f77fd9ffb1faf753cb0e016526abe
Sarah Lindmar <srhcrete@gmail.com>
2017-09-15 08:37:22 -0700

    set up project files

---
+++ spec/word_central_integration_spec.rb
@@ -0,0 +1,14 @@
+require('capybara/rpsec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('') do
+  it('stores and displays information about user words') do
+    visit('/')
+    fill_in('word', :word => 'fragment')
+    fill_in('definition', :definition => ['a small part broken or seperated off something', 'to break or cause to break into fragments'])
+    click_button('Add word and definitions')
+    expect(page).to have_content('fragment')
+  end
+end
